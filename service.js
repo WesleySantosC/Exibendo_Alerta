e7a620676c1ebefce6b1775c23aa9aed66c62902
WesleySantosC <155553388+WesleySantosC@users.noreply.github.com>
2024-06-29 20:26:42 -0300

    Update service.js
    As condições foram retiradas, poque nunca iria cair no else, somente no IF, pois, não havia nenhuma validação.

--- service.js
+++ service.js
@@ -1,9 +1,5 @@
 let botao = document.querySelector("button");
 
 botao.addEventListener("click", function() {
-  if(botao) {
     alert('Você clicou no botão!');
-  } else {
-    alert('Houve um erro!');
-  }
 });
